920857a698ccedd6752e18ffd1d17cbebd58ebe2
priyankamaya <pinkakrishnan95@gmail.com>
2023-08-09 07:12:12 +0000

    Updated search

--- src/test/java/webdriverSampleB35/GooglePageTest.java
+++ src/test/java/webdriverSampleB35/GooglePageTest.java
@@ -1,6 +1,8 @@
 package webdriverSampleB35;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GooglePageTest {
@@ -9,7 +11,10 @@ public class GooglePageTest {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("http://www.google.com/");
+		WebElement srcbox= driver.findElement(By.id("APjFqb"));
+		srcbox.sendKeys("Java Tutorial");
 		driver.close();
 		
 
